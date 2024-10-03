3746b87d4f8f6a3e72bbcb519629acd01535a657
oinktech <oinktech2024@gmail.com>
2024-10-03 12:56:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -228,4 +228,4 @@ def get_languages_ja():
     return jsonify(supported_languages_ja)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
